0d0e1cc2c37b9dd8ae831131053e5111394ef02f
mikadominguezz <mikadominguezz2004@gmail.com>
2025-09-17 08:51:59 -0300

    react router implementado

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import StarRatingDemo from './StarRatingDemo.jsx';
 
+function Home() {
+  return (
+    <div className="App">
+      <h1>Bienvenido a la demo</h1>
+      <Link to="/calificacion">Ir a calificación de productos</Link>
+    </div>
+  );
+}
+
 function App() {
-  return <StarRatingDemo />;
+  return (
+    <BrowserRouter>
+      <nav style={{ margin: '16px' }}>
+        <Link to="/" style={{ marginRight: '16px' }}>Inicio</Link>
+        <Link to="/calificacion">Calificación</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/calificacion" element={<StarRatingDemo />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
