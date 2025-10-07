61a801399fbfe03f0f29c053328b73577c4571f1
mikadominguezz <mikadominguezz2004@gmail.com>
2025-10-07 14:23:44 -0300

    correccion de codigo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-function Navbar({ inputValue, setInputValue, handleInputKeyDown }) {
+function Navbar() {
+  const [text, setText] = useState(()=>{
+    return localStorage.getItem('navbarInput') || '';
+  })
+
+  useEffect(() => {
+    localStorage.setItem('navbarInput', text);
+  }, [text]);
+
   return (
     <nav className="navbar" style={{ marginBottom: 32 }}>
       <input
         type="text"
         placeholder="Escribe algo..."
-        value={inputValue}
-        onChange={e => setInputValue(e.target.value)}
-        onKeyDown={handleInputKeyDown}
+        value={text}
+        onChange={e => { setText(e.target.value); }}
         style={{ padding: '8px 16px', borderRadius: 6, border: '1px solid #ccc', fontSize: '1rem', width: 220 }}
       />
     </nav>
